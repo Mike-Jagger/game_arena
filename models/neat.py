bd9e61389c0e056680ec83e7af447d847109bcec
mikewolfer37@gmail.com <mikewolfer37@gmail.com>
2026-08-11 09:13:44 -0600

    adding required ML algo class abstract methods to initial models

--- models/neat.py
+++ models/neat.py
@@ -1,3 +1,12 @@
 class NEATAgent:
     def __init__(self, config_path):
         pass
+
+    def get_action(self, state, is_training):
+        pass
+
+    def save(self, filepath):
+        pass
+
+    def load(self, filepath):
+        pass
--- models/q_learning.py
+++ models/q_learning.py
@@ -1,3 +1,12 @@
 class QLearningAgent:
     def __init__(self, action_size=3, lr=0.001, gamma=0.9, epsilon=1.0, epsilon_decay=0.995, min_epsilon=0.01):
         pass
+
+    def get_action(self, state, is_training):
+        pass    
+
+    def save(self, filepath):
+        pass
+
+    def load(self, filepath):
+        pass
