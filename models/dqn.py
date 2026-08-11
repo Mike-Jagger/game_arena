fad21f188e7523390cb4cfb0dd1ac6cb21dbb69e
mikewolfer37@gmail.com <mikewolfer37@gmail.com>
2026-08-11 09:09:13 -0600

    adding initial look of what an ML algo class should look like

--- models/dqn.py
+++ models/dqn.py
@@ -1,3 +1,12 @@
 class DQNAgent:
     def __init__(self, input_dim=11, output_dim=3, lr=0.001, gamma=0.9):
         pass
+
+    def get_action(self, state, is_training):
+        pass
+
+    def save(self, filepath):
+        pass
+
+    def load(self, filepath):
+        pass
