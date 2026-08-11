973a4f88d49c6726cb9232e4099e703157142ef8
mikewolfer37@gmail.com <mikewolfer37@gmail.com>
2026-08-11 07:30:29 -0600

    adding reference implementation repo; implementation of constructor and reset functions

--- games/snake.py
+++ games/snake.py
@@ -1,3 +1,5 @@
+# This is hugely based on this implementation of an AI Snake game: https://github.com/SandeepPadhi/AI_That_Plays_Snake
+
 import pygame
 import random
 import numpy as np
@@ -12,3 +14,23 @@ class Direction(Enum):
     LEFT = 2
     UP = 3
     DOWN = 4
+
+class SnakeGame:
+    def __init__(self, width=400, height=400):
+        self.w = width
+        self.h = height
+        self.reset()
+
+    def reset(self):
+        self.direction = Direction.RIGHT
+        self.head = Point(self.w // 2, self.h // 2)
+        self.snake = [
+            self.head,
+            Point(self.head.x - BLOCK_SIZE, self.head.y),
+            Point(self.head.x - (2 * BLOCK_SIZE), self.head.y)
+        ]
+        self.score = 0
+        self.food = None
+        self._place_food()
+        self.frame_iteration = 0
+        return self.get_satte()
