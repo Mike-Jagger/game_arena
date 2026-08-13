3ee9f7c3e08b401cf9121878be358c02425e03b7
Kofi Mason <kofi.sesay@ashesi.edu.gh>
2026-08-13 20:32:38 +0000

    Implement neat_agent

--- models/neat_agent.py
+++ models/neat_agent.py
@@ -1,12 +1,27 @@
+import neat
+import pickle
+import numpy as np
+
 class NEATAgent:
     def __init__(self, config_path):
-        pass
+        self.config = neat.Config(
+            neat.DefaultGenome,
+            neat.DefaultReproduction,
+            neat.DefaultSpeciesSet,
+            neat.DefaultStagnation,
+            config_path
+        )
+        self.net = None
 
-    def get_action(self, state, is_training):
-        pass
+    def get_action(self, state):
+        output = self.net.activate(state)
+        return int(np.argmax(output))
 
-    def save(self, filepath):
-        pass
+    def save(self, winner_genome, filepath):
+        with open(filepath, 'wb') as f:
+            pickle.dump(winner_genome, f)
 
     def load(self, filepath):
-        pass
+        with open(filepath, 'rb') as f:
+            winner = pickle.load(f)
+        self.net = neat.nn.FeedForwardNetwork.create(winner, self.config)
