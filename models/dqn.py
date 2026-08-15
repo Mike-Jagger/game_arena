05b372b583005b50edf30ecf908f43c8042e8764
94Martha <martha.maker@ashesi.edu.gh>
2026-08-15 09:21:10 +0000

    implement function init

--- models/dqn.py
+++ models/dqn.py
@@ -10,6 +10,20 @@ class DQNAgent:
         x = torch.relu(self.linear1(x))
         x = torch.relu(self.linear2(x))
         return self.linear3(x)
+    class DQNAgent:
+    def __init__(self, input_dim=11, output_dim=3, lr=0.001, gamma=0.9):
+        self.gamma = gamma
+        self.output_dim = output_dim
+        self.memory = deque(maxlen=20000)
+        self.batch_size = 64
+        self.epsilon = 1.0
+        self.epsilon_decay = 0.995
+        self.min_epsilon = 0.01
+
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model = LinearQNet(input_dim, 128, output_dim).to(self.device)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
+        self.criterion = nn.MSELoss()
     
 
     def get_action(self, state, is_training):
