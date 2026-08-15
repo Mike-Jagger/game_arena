4a7465d20d8ad4d927a5965e16671dcd35ebb0fe
94Martha <martha.maker@ashesi.edu.gh>
2026-08-15 09:16:06 +0000

    implement function forward

--- models/dqn.py
+++ models/dqn.py
@@ -1,18 +1,16 @@
 class DQNAgent:
-    def __init__(self, input_dim=11, output_dim=3, lr=0.001, gamma=0.9):
-        self.gamma = gamma
-        self.output_dim = output_dim
-        self.memory = deque(maxlen=20000)
-        self.batch_size = 64
-        self.epsilon = 1.0
-        self.epsilon_decay = 0.995
-        self.min_epsilon = 0.01
+  class LinearQNet(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(LinearQNet, self).__init__()
+        self.linear1 = nn.Linear(input_dim, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
+        self.linear3 = nn.Linear(hidden_dim, output_dim)
 
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model = LinearQNet(input_dim, 128, output_dim).to(self.device)
-        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
-        self.criterion = nn.MSELoss()
-      
+    def forward(self, x):
+        x = torch.relu(self.linear1(x))
+        x = torch.relu(self.linear2(x))
+        return self.linear3(x)
+    
 
     def get_action(self, state, is_training):
         if is_training and random.random() < self.epsilon:
@@ -26,7 +24,7 @@ class DQNAgent:
 
     def save(self, filepath):
         torch.save(self.model.state_dict(), filepath)
-        
+
         
 
     def load(self, filepath):
