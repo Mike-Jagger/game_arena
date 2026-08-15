76a554fd7aee0f22af9d1421ee73bd7c0ad7bc0a
94Martha <martha.maker@ashesi.edu.gh>
2026-08-15 10:47:33 +0000

    implement function train_step

--- models/dqn.py
+++ models/dqn.py
@@ -12,7 +12,7 @@ class DQNAgent:
         return self.linear3(x)
     
     class DQNAgent:
-        
+
     def __init__(self, input_dim=11, output_dim=3, lr=0.001, gamma=0.9):
         self.gamma = gamma
         self.output_dim = output_dim
@@ -39,7 +39,33 @@ class DQNAgent:
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
 
-        
+     def train_step(self):
+        if len(self.memory) < self.batch_size:
+            return 0.0
+        mini_sample = random.sample(self.memory, self.batch_size)
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+
+        states_t = torch.tensor(np.array(states), dtype=torch.float).to(self.device)
+        next_states_t = torch.tensor(np.array(next_states), dtype=torch.float).to(self.device)
+        actions_t = torch.tensor(actions, dtype=torch.long).unsqueeze(1).to(self.device)
+        rewards_t = torch.tensor(rewards, dtype=torch.float).to(self.device)
+        dones_t = torch.tensor(dones, dtype=torch.bool).to(self.device)
+
+        pred = self.model(states_t).gather(1, actions_t).squeeze()
+        next_q = self.model(next_states_t).max(1)[0]
+        next_q[dones_t] = 0.0
+        target = rewards_t + self.gamma * next_q
+
+        loss = self.criterion(pred, target.detach())
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        if self.epsilon > self.min_epsilon:
+            self.epsilon *= self.epsilon_decay
+
+        return loss.item()
+   
 
     def save(self, filepath):
         torch.save(self.model.state_dict(), filepath)
