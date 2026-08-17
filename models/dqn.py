5c83dace99de4eb4431ea50d2f281e082d2fa4fa
mikewolfer37@gmail.com <mikewolfer37@gmail.com>
2026-08-17 01:01:12 -0600

    reimplimenting RL models cause of freaking incomplete implementation

--- models/dqn.py
+++ models/dqn.py
@@ -1,5 +1,11 @@
-class DQNAgent:
-  class LinearQNet(nn.Module):
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import random
+from collections import deque
+import numpy as np
+
+class LinearQNet(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(LinearQNet, self).__init__()
         self.linear1 = nn.Linear(input_dim, hidden_dim)
@@ -10,9 +16,8 @@ class DQNAgent:
         x = torch.relu(self.linear1(x))
         x = torch.relu(self.linear2(x))
         return self.linear3(x)
-    
-    class DQNAgent:
 
+class DQNAgent:
     def __init__(self, input_dim=11, output_dim=3, lr=0.001, gamma=0.9):
         self.gamma = gamma
         self.output_dim = output_dim
@@ -26,20 +31,19 @@ class DQNAgent:
         self.model = LinearQNet(input_dim, 128, output_dim).to(self.device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
         self.criterion = nn.MSELoss()
-    
 
-    def get_action(self, state, is_training):
+    def get_action(self, state, is_training=True):
         if is_training and random.random() < self.epsilon:
             return random.randint(0, self.output_dim - 1)
         state_t = torch.tensor(state, dtype=torch.float).to(self.device)
         with torch.no_grad():
             prediction = self.model(state_t)
         return int(torch.argmax(prediction).item())
-    
+
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
 
-     def train_step(self):
+    def train_step(self):
         if len(self.memory) < self.batch_size:
             return 0.0
         mini_sample = random.sample(self.memory, self.batch_size)
@@ -65,14 +69,10 @@ class DQNAgent:
             self.epsilon *= self.epsilon_decay
 
         return loss.item()
-   
 
     def save(self, filepath):
         torch.save(self.model.state_dict(), filepath)
 
-        
-
     def load(self, filepath):
         self.model.load_state_dict(torch.load(filepath, map_location=self.device))
         self.model.eval()
-      
--- models/q_learning.py
+++ models/q_learning.py
@@ -1,5 +1,9 @@
+import pickle
+import numpy as np
+import random
+
 class QLearningAgent:
-       def _init_(self, action_size=3, lr=0.001, gamma=0.9, epsilon=1.0, epsilon_decay=0.995, min_epsilon=0.01):
+    def __init__(self, action_size=3, lr=0.001, gamma=0.9, epsilon=1.0, epsilon_decay=0.995, min_epsilon=0.01):
         self.action_size = action_size
         self.lr = lr
         self.gamma = gamma
@@ -7,18 +11,14 @@ class QLearningAgent:
         self.epsilon_decay = epsilon_decay
         self.min_epsilon = min_epsilon
         self.q_table = {}
-        
 
-    def get_action(self, state, is_training):
+    def get_action(self, state, is_training=True):
         state_key = tuple(state) if isinstance(state, (list, np.ndarray)) else state
         if is_training and random.random() < self.epsilon:
             return random.randint(0, self.action_size - 1)
         if state_key not in self.q_table:
             self.q_table[state_key] = np.zeros(self.action_size)
         return int(np.argmax(self.q_table[state_key]))
-        
-           
-
 
     def update(self, state, action, reward, next_state, done):
         state_key = tuple(state) if isinstance(state, (list, np.ndarray)) else state
@@ -36,11 +36,10 @@ class QLearningAgent:
         if done and self.epsilon > self.min_epsilon:
             self.epsilon *= self.epsilon_decay
 
-    def  save(self, filepath)
-         with open(filepath, 'wb') as f:
+    def save(self, filepath):
+        with open(filepath, 'wb') as f:
             pickle.dump(self.q_table, f)
 
-
     def load(self, filepath):
-         with open(filepath, 'rb') as f:
+        with open(filepath, 'rb') as f:
             self.q_table = pickle.load(f)
