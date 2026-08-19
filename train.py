9744312f5daf205febede41fb058d205f88a089a
mikewolfer37@gmail.com <mikewolfer37@gmail.com>
2026-08-19 00:03:23 -0600

    train with neat based on command line args

--- train.py
+++ train.py
@@ -145,3 +145,5 @@ if __name__ == '__main__':
             train_snake_qlearning(args.episodes)
         elif args.model == 'dqn':
             train_snake_dqn(args.episodes)
+        elif args.model == 'neat':
+            train_snake_neat(args.episodes)
